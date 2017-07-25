4a74f977e4a4acd1fa2a0001f77f07188be65140
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-07-25 19:49:55 +0200

    Create 3.jsx

---
+++ 3.jsx
@@ -0,0 +1,14 @@
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 1};
+  }
+  
+  render() {
+    return (
+      <button>{this.state.counter}</button>
+    );
+  }
+}
+
+ReactDOM.render(<Button />, mountNode);

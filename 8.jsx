5be18bcbdbed6dd8783acd8facc3833ba1f5ae5a
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-07-26 20:59:58 +0200

    Update 8.jsx

--- 8.jsx
+++ 8.jsx
@@ -1,23 +1,23 @@
 const Card = (prop) => {
-	return (
-    <div>
-      <img width='100' src={prop.avatar_url} />
-      <div style={{fontWeight: 'bold'}}>
-        {prop.name}
-      </div>
-      <div>
-        {prop.company}
-      </div>
-      <div>
-        {prop.bio}
-      </div>
-    </div>
-  )
+    return (
+		<div>
+		  <img width='100' src={prop.avatar_url} />
+		  <div style={{fontWeight: 'bold'}}>
+			{prop.name}
+		  </div>
+		  <div>
+			{prop.company}
+		  </div>
+		  <div>
+			{prop.bio}
+		  </div>
+		</div>
+    )
 }
 
 let user = {
 	avatar_url: 'https://avatars2.githubusercontent.com/u/12496512?v=4',
-  name: 'Roman Krasavtsev',
-  bio: 'Passionate Software Engineer with DevOps mindset'
+  	name: 'Roman Krasavtsev',
+  	bio: 'Passionate Software Engineer with DevOps mindset'
 }
 ReactDOM.render(<Card {...user} />, mountNode);
